f4681e9abad08152cb78d6c09f3eebbe0f384469
Nicolas <nicksiuxs@gmail.com>
2021-02-11 21:37:08 -0500

    ADD: form component to the app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import Header from './components/Header/Header'
+import Form from './components/Form/Form'
+
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+const ContainerForm = styled.div`
+  background-color: #FFFFFF;
+  padding: 3rem;
+`;
 
 function App() {
   return (
-    <Header title='Cotizador de seguros' />
+    <Container>
+      <Header title='Cotizador de seguros' />
+      <ContainerForm>
+        <Form/>
+      </ContainerForm>
+    </Container>
   );
 }
 

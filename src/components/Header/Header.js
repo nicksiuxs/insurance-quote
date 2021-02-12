50684444ff110a91a986057707dfa99bfb40768e
Nicolas <nicksiuxs@gmail.com>
2021-02-11 20:20:12 -0500

    ADD: header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const ContainerHeader = styled.header`
+    background-color: #26C6DA;
+    padding: 10px;
+    font-weight: bold;
+    color: #FFFFFF;
+`;
+
+
+const TextHeader = styled.h1`
+    font-size: 2rem;
+    margin: 0;
+    font-family: 'Slabo 27px', serif;
+    text-align: center;
+`;
 
 const Header = ({title}) => {
     return ( 
-        <header>
-            <h1>{title}</h1>
-        </header>
+        <ContainerHeader>
+            <TextHeader>{title}</TextHeader>
+        </ContainerHeader>
     );
 }
  

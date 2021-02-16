8b58a6df5a78bb07ab4c42be2ecd4fbf1be07a66
Nicolas <nicksiuxs@gmail.com>
2021-02-15 22:03:18 -0500

    ADD: styled componentes were adde to the form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,20 +1,62 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const Field = styled.div`
+    display: flex;
+    margin-bottom: 1rem;
+    align-items: center;
+`;
+
+const Label = styled.label`
+    flex: 0 0 100px;
+`;
+
+const Select = styled.select`
+    display: block;
+    width: 100%;
+    padding: 1rem;
+    border: 1px solid #e1e1e1;
+    -webkit-appearance: none;
+`;
+
+const InputRadio = styled.input`
+    margin: 0 1rem;
+
+`;
+
+const Button = styled.button`
+    background-color: #00838F;
+    font-size: 16px;
+    width: 100%;
+    padding: 1rem;
+    color: #FFF;
+    text-transform: uppercase;
+    font-weight: bold;
+    border: none;
+    transition: background-color .3s ease;
+    margin-top: 2rem;
+
+    &:hover{
+        background-color: #26C6DA;
+        cursor: pointer;
+    }
+`;
 
 const Form = () => {
-    return ( 
+    return (
         <form>
-            <div>
-                <label>Marca</label>
-                <select>
+            <Field>
+                <Label>Marca</Label>
+                <Select>
                     <option value="">--- seleccione ---</option>
                     <option value="americano">Americano</option>
                     <option value="europeo">Europeo</option>
                     <option value="asiatico">Asiático</option>
-                </select>
-            </div>
-            <div>
-                <label>Año</label>
-                <select>
+                </Select>
+            </Field>
+            <Field>
+                <Label>Año</Label>
+                <Select>
                     <option value="">--- seleccione ---</option>
                     <option value="2021">2021</option>
                     <option value="2020">2020</option>
@@ -23,24 +65,24 @@ const Form = () => {
                     <option value="2017">2017</option>
                     <option value="2016">2016</option>
                     <option value="2015">2015</option>
-                </select>
-            </div>
-            <div>
-                <label>Plan</label>
-                <input 
+                </Select>
+            </Field>
+            <Field>
+                <Label>Plan</Label>
+                <InputRadio
                     type="radio"
                     name="plan"
                     value="basico"
                 />Básico
-                <input 
+                <InputRadio
                     type="radio"
                     name="plan"
                     value="completo"
                 />Completo
-            </div>
-            <button type ="button">Cotizar</button>
+            </Field>
+            <Button type="button">Cotizar</Button>
         </form>
-     );
+    );
 }
- 
+
 export default Form;
